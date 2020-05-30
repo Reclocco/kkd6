cd4740904e7cca365d38792dedb8808192c85b41
Michał Reclik <michal-reclik@wp.pl>
2020-05-30 02:59:28 +0200

    blob

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import copy
 import math
-import sys
 
 
-def read_encoded():
+def decode():
     encoded = open('encoded.tga', "rb")
     out = open('out.tga', 'wb')
     out.write(encoded.read(12))
@@ -18,28 +17,69 @@ def read_encoded():
 
     x = int.from_bytes(x, byteorder='little')
     y = int.from_bytes(y, byteorder='little')
-    print(x, y)
 
-    prev = [0, 0, 0]
-    for i in range(x * y):
-        pixel = [int.from_bytes(encoded.read(1), byteorder='little'),
-                 int.from_bytes(encoded.read(1), byteorder='little'),
-                 int.from_bytes(encoded.read(1), byteorder='little')]
+    bottom_encoded = []
+    top_encoded = []
 
-        if i % 2 == 1:
-            print(pixel)
-            prev = [(prev[k] + pixel[k] - 170) for k in range(3)]
-            print(prev)
+    for i in range(int(x * y / 2)):
+        top_pixel = [int.from_bytes(encoded.read(1), byteorder='little'),
+                     int.from_bytes(encoded.read(1), byteorder='little'),
+                     int.from_bytes(encoded.read(1), byteorder='little')]
+        top_encoded.append([top_pixel[k] - 128 for k in range(3)])
+
+        bottom_pixel = [int.from_bytes(encoded.read(1), byteorder='little'),
+                        int.from_bytes(encoded.read(1), byteorder='little'),
+                        int.from_bytes(encoded.read(1), byteorder='little')]
+        if i == 0:
+            bottom_encoded.append(bottom_pixel)
+        else:
+            bottom_encoded.append([bottom_pixel[k] - 128 + bottom_encoded[i-1][k] for k in range(3)])
+
+        print(top_encoded[i], bottom_encoded[i], bottom_pixel)
+
+    for i in range(int(x * y / 2)):
+        if i != 0:
             for k in range(3):
-                out.write((prev[k]).to_bytes(1, 'little'))
+                try:
+                    out.write((int(bottom_encoded[i][k] - top_encoded[i][k])).to_bytes(1, 'little'))
+                except OverflowError:
+                    if int(bottom_encoded[i][k] - top_encoded[i][k]) < 0:
+                        out.write((0).to_bytes(1, 'little'))
+                    else:
+                        out.write((255).to_bytes(1, 'little'))
+
+            for k in range(3):
+                try:
+                    out.write((int(bottom_encoded[i][k] + top_encoded[i][k])).to_bytes(1, 'little'))
+                except OverflowError:
+                    if int(bottom_encoded[i][k] + top_encoded[i][k]) < 0:
+                        out.write((0).to_bytes(1, 'little'))
+                    else:
+                        out.write((255).to_bytes(1, 'little'))
+
         else:
             for k in range(3):
-                out.write((pixel[k]).to_bytes(1, 'little'))
+                try:
+                    out.write((int(bottom_encoded[i][k] + top_encoded[i][k])).to_bytes(1, 'little'))
+                except OverflowError:
+                    if int(bottom_encoded[i][k] + top_encoded[i][k]) < 0:
+                        out.write((0).to_bytes(1, 'little'))
+                    else:
+                        out.write((255).to_bytes(1, 'little'))
+
+    for k in range(3):
+        try:
+            out.write((int(bottom_encoded[int(x * y / 2) - 1][k] + top_encoded[int(x * y / 2) - 1][k])).to_bytes(1, 'little'))
+        except OverflowError:
+            if int(bottom_encoded[int(x * y / 2) - 1][k] + top_encoded[int(x * y / 2) - 1][k]) < 0:
+                out.write((0).to_bytes(1, 'little'))
+            else:
+                out.write((255).to_bytes(1, 'little'))
 
     out.write(encoded.read(26))
 
 
-def read_tga(bits):
+def encode(bits):
     sectors = [int(math.pow(2, 8 - bits) / 2 + i * math.pow(2, 8 - bits)) for i in range(int(math.pow(2, bits)))]
 
     f = open('ex.tga', "rb")
@@ -58,9 +98,7 @@ def read_tga(bits):
     y = int.from_bytes(y, byteorder='little')
 
     bottom = []
-    bottom_encoded = []
     top = []
-    top_encoded = []
 
     b_prev = 0
     g_prev = 0
@@ -79,51 +117,23 @@ def read_tga(bits):
         top.append([(b_byte - b_prev) / 2, (g_byte - g_prev) / 2, (r_byte - r_prev) / 2])
         bottom.append([(b_byte + b_prev) / 2, (g_byte + g_prev) / 2, (r_byte + r_prev) / 2])
 
-        top_encoded.append([belong(sectors, top[i][k] + 128) - 128 for k in range(3)])
+        # print([bottom[i][k] - bottom[i - 1][k] + 128 for k in range(3)])
 
-        # try:
-        #     diff = [bottom[i][k] - bottom[i - 1][k] + 128 for k in range(3)]
-        #
-        #     bottom_encoded.append(diff)
-        # except IndexError:
-        #     bottom_encoded.append(bottom[i])
+        if i % 2 == 0:
+            for k in range(3):
+                encoded.write((belong(sectors, top[i][k] + 128)).to_bytes(1, 'little'))
+
+            try:
+                for k in range(3):
+                    encoded.write((int(bottom[i][k] - bottom[i - 1][k] + 128)).to_bytes(1, 'little'))
+
+            except IndexError:
+                for k in range(3):
+                    encoded.write((int(bottom[i][k])).to_bytes(1, 'little'))
 
         b_prev = copy.deepcopy(b_byte)
         g_prev = copy.deepcopy(g_byte)
         r_prev = copy.deepcopy(r_byte)
-
-    for i in range(int(x * y / 2)):
-        if i != 0:
-            for k in range(3):
-                try:
-                    encoded.write((int(bottom[2 * i][k] - top_encoded[2 * i][k])).to_bytes(1, 'little'))
-                except OverflowError:
-                    if int(bottom[2 * i][k] - top_encoded[2 * i][k]) < 0:
-                        encoded.write((0).to_bytes(1, 'little'))
-                    else:
-                        encoded.write((255).to_bytes(1, 'little'))
-
-            for k in range(3):
-                try:
-                    encoded.write((int(bottom[2 * i][k] + top_encoded[2 * i][k])).to_bytes(1, 'little'))
-                except OverflowError:
-                    if int(bottom[2 * i][k] + top_encoded[2 * i][k]) < 0:
-                        encoded.write((0).to_bytes(1, 'little'))
-                    else:
-                        encoded.write((255).to_bytes(1, 'little'))
-
-        else:
-            for k in range(3):
-                try:
-                    encoded.write((int(bottom[2 * i][k] + top_encoded[2 * i][k])).to_bytes(1, 'little'))
-                except OverflowError:
-                    if int(bottom[2 * i][k] + top_encoded[2 * i][k]) < 0:
-                        encoded.write((0).to_bytes(1, 'little'))
-                    else:
-                        encoded.write((255).to_bytes(1, 'little'))
-
-    for k in range(3):
-        encoded.write((int(bottom[x * y - 1][k] + top_encoded[x * y - 1][k])).to_bytes(1, 'little'))
 
     encoded.write(f.read(26))
 
@@ -145,8 +155,8 @@ def belong(sectors, x):
 
 
 def main():
-    read_tga(3)
-    # read_encoded()
+    encode(3)
+    decode()
 
 
 main()
